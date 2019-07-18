09c9d132ff548ad1cb9bccb32245c61bfe6695da
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-17 21:47:28 -0400

    Resized text

--- src/components/styled-components/ReadableText.js
+++ src/components/styled-components/ReadableText.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const ReadableText = styled.button`
   margin-top: 2rem !important;
-  font-size: 1.5rem;
+  font-size: 1rem;
   line-height: 3.5rem;
   padding: 2rem;
   text-align: left;
